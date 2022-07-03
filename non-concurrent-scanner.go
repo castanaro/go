531d6490564ac4f2098e1b80bf3b7890c4506a94
Michael Castanaro <66240320+castanaro@users.noreply.github.com>
2022-07-02 22:43:33 -0700

    Create non-concurrent-scanner.go

---
+++ non-concurrent-scanner.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	for i := 1; i <= 1024; i++ {
+		address := fmt.Sprintf("scanme.nmap.org:%d", i)
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			//port is filtered or closed
+			continue
+		}
+
+		conn.Close()
+		fmt.Printf("%d open\n", i)
+
+	}
+}
